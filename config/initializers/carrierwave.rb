05d8a38dc38e81f49f3533aedd4abbc7f56f64f9
Riccardo Rizzo <r.riki@tiscali.it>
2015-10-26 14:37:31 +0000

    Image upload in AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,8 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+      :host => 's3.amazonaws.com',
+      :region => 'ireland'
   }
   config.fog_directory  = ENV['S3_BUCKET']
 end
