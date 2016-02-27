85087a67094474285c95981be21d376aca60402c
Riccardo Rizzo <r.riki@tiscali.it>
2016-02-27 11:00:03 +0000

    Added get_category_image API

--- config/routes.rb
+++ config/routes.rb
@@ -70,6 +70,7 @@ Rails.application.routes.draw do
   get 'api/delete_all_messages'
   get 'api/get_num_of_messages'
   get 'api/get_category_image'
+  get 'api/get_categories'
 
   get 'eula', :to => 'welcome#eula'
   get 'start', :to => 'application#start'
