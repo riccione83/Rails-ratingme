b481cc42ef9c762b31ae0dc5e9c7ff9bd1c07bce
Riccardo Rizzo <r.riki@tiscali.it>
2016-03-05 09:02:14 +0000

    Added production apns

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -5,7 +5,7 @@ APNS.host = 'gateway.push.apple.com'
 APNS.port = 2195 
 # this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
 
-APNS.pem  = 'config/cert_production.pem'
+APNS.pem  = 'config/production.pem'
 # this is the file you just created
 
 APNS.pass = ''
