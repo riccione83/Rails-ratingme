2944d1f5adb9da7430a866d69b62583cbf6d221d
Riccardo Rizzo <r.riki@tiscali.it>
2016-03-11 21:12:55 +0000

    apn mod

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -1,4 +1,4 @@
-APNS.host = 'gateway.sandbox.push.apple.com' 
+APNS.host = 'gateway.push.apple.com' 
 # gateway.sandbox.push.apple.com is default and only for development
 # gateway.push.apple.com is only for production
 
